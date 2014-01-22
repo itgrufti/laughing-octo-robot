233a7f22163c6b441072f199b2c50847ff0adffe
Lukas <k3ll4gr0up@gmail.com>
2014-01-22 11:57:22 +0100

    Update main.c
    Anscheinend müssen bei den '||' doch keine Klammern hin, hab mich in der Doku verlesen.
    
    Aus irgendeinem Grund funktioniert das mit den Argumenten nicht richtig.

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-int main ( int argc, char *argv[] )
+int main ( int argc, char *argv[] ) // Ich glaube, hier ist etwas falsch, ich komme aber nicht drauf, was.
 	{
 		// Wenn das erste Argument -h oder --help ist, Hilfe anzeigen.
-		if( argv[1] == "-h" ) || ( argv[1] == "--help" ){
+		if( argv[1] == "-h" || argv[1] == "--help" ){
 			printf(" Hilfe ausgeführt!\n");
 			printf("-h, --help	Hilfe zeigen.\n");
 			printf("-t, --test  Testfunktion");
 			return 0;
 		}
 		// Wenn das erste Argument -t oder --test ist, gib etwas aus.
-		else if( argv[1] == "-t" ) || ( argv[1] == "--test" ){
-			printf("Test Erfolgreich");
+		else if( argv[1] == "-t" || argv[1] == "--test" ){
+			printf("Test Erfolgreich\n");
 			return 0;
 		}
 		else{
-			printf("Normaler Start");
+			char eins, zwei;
+			eins = argv[0];
+			zwei = argv[1];
+			printf("Normaler Start\n");
+			printf("%d\n", eins);
+			printf("%d\n", zwei);
+			return 0;
 		}
 }
