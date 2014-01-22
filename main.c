8f8bd2dc208b4e529f76f7ff1dabbbc3ef85e992
Lukas <k3ll4gr0up@gmail.com>
2014-01-22 11:40:56 +0100

    Update main.c
    Wäre es möglich/sinnvoll, dass ALLE angegebenen Argumente auf das Vorkommen des jeweiligen Arguments geprüft werden?
    
    Also wenn man im Terminal eintippt:
    ```
    $ ./programm -bla -h
    ```
    dass das Programm dann trotzdem nur die Hilfe anzeigt, obwohl -h als zweites Argument angegeben ist.

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
- 
-int main(void)
-{
-    printf("Hallo Welt!\n");
-    return 0;
+
+int main ( int argc, char *argv[] )
+	{
+		// Wenn das erste Argument -h oder --help ist, Hilfe anzeigen.
+		if( argv[1] == "-h" ) || ( argv[1] == "--help" ){
+			printf(" Hilfe ausgeführt!\n");
+			printf("-h, --help	Hilfe zeigen.\n");
+			printf("-t, --test  Testfunktion");
+			return 0;
+		}
+		// Wenn das erste Argument -t oder --test ist, gib etwas aus.
+		else if( argv[1] == "-t" ) || ( argv[1] == "--test" ){
+			printf("Test Erfolgreich");
+			return 0;
+		}
+		else{
+			printf("Normaler Start");
+		}
 }
